3fb4f0ac96b4183f297e312001865e1e47e1ba05
EvGus52 <evgenijguskov247@gmail.com>
2025-02-01 17:50:08 +0300

    completed the tasks hw3.js

---
+++ hw3.js
@@ -0,0 +1,87 @@
+//Задание № 1
+// let password = 'пароль';
+// let passwordInput = prompt('Введите пароль');
+// if (password===passwordInput) {
+//     alert ('Пароль введен верно');
+// } else {
+//     alert ('Пароль введен неправильно');
+// }
+
+//Задание № 2
+// let c = Number(prompt('Введите число'));
+// console.log (c > 0 && c < 10 ? 'Верно' : 'Неверно');
+
+//Задание № 3
+// let d = Number(prompt('Введите первое число'));
+// let e = Number(prompt('Введите второе число'));
+// console.log (d > 100 || e > 100 ? 'Верно' : 'Неверно');
+
+//Задание № 4
+// let a = '2';
+// let b = '3';
+// // Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
+// alert(parseInt(a) + parseInt(b));
+
+//Задание № 5
+// let monthNumber = Number(prompt("Введите номер месяца"));
+// switch (monthNumber) {
+//     case 12:
+//     console.log('Зима');
+//         break;
+//     case 1:
+//     console.log('Зима');
+//         break;
+//     case 2:
+//     console.log('Зима');
+//         break;
+//     case 3:
+//     console.log('Весна');
+//         break;
+//     case 4:
+//     console.log('Весна');
+//         break;
+//     case 5:
+//     console.log('Весна');
+//         break;
+//     case 6:
+//     console.log('Лето');
+//         break;
+//     case 7:
+//     console.log('Лето');
+//         break;
+//     case 8:
+//     console.log('Лето');
+//         break;
+//     case 9:
+//     console.log('Осень');
+//         break;
+//     case 10:
+//     console.log('Осень');
+//         break;
+//     case 11:
+//     console.log('Осень');
+//         break;
+//     default:
+//     console.log('такого месяца не существует');
+// }
+
+//Дополнительные задания
+//Задание № 1
+// let num = prompt("Пожалуйста, введите любое число");
+// if (isNaN(num)) {
+//     alert("Вы ввели не число!");
+//   }
+// alert (num % 2 == 0 ? 'Четное число!' : 'Нечетное число!');
+
+//Задание № 2
+// let clientOS = Number(prompt("Какое устройство вы используете (0 — iOS, 1 — Android"));
+// switch (clientOS) {
+//     case 0:
+//     alert('Установите версию приложения для iOS по ссылке');
+//         break;
+//     case 1:
+//     alert('Установите версию приложения для Android по ссылке');
+//         break;
+//     default:
+//         alert ('Вы ввели некорректное значение!');
+// }
