99f28c9c6b82b46df396bc36c8a4814cef5ac635
EvGus52 <evgenijguskov247@gmail.com>
2025-02-01 12:53:25 +0300

    finished hw2.js

---
+++ hw2.js
@@ -0,0 +1,69 @@
+ // Задание № 1
+// let a = 10;
+// alert (a);
+// a = 20;
+// alert (a);
+
+//Задание № 2
+// const firstIphoneYear = 2007;
+// alert (`Год выпуска первого iPhone - ${firstIphoneYear} год`);
+
+//Задание № 3
+// const nameJavaCreator = 'Брендан Эйх';
+// alert (`Имя создателя языка JavaScript - ${nameJavaCreator}`);
+
+//Задание № 4
+// let a = Number(10);
+// let b = Number(2);
+// alert (a + b);
+// alert (a - b);
+// alert (a * b);
+// alert (a / b);
+
+//Задание № 5
+// let a = Number(2);
+// let result = a ** 5;
+// alert (`2 в 5-й степени = ${result}`);
+
+//Задание № 6
+// let a = Number(9);
+// let b = Number(2);
+// let result = a % b;
+// alert (`Остаток от деления 9 на 2 = ${result}`);
+
+//Задание № 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
+
+//Задание № 8
+// let age = Number(prompt('Сколько вам лет?'));
+// alert (`Ваш возраст ${age} лет`);
+
+//Задание № 9
+// const user = {
+//     name: 'Борис',
+//     age: Number(33),
+//     isAdmin: false
+// };
+
+//Задание № 10
+// let userName = (prompt('Введите ваше имя'));
+// alert (`Привет, ${userName}!`);
+   
+//Доп. задание
+// let userNum = Number((prompt('Загадайте любое число')));
+// let a = userNum * 2;
+// alert (`Удвоенное загаданное число = ${a}`);
+// let b = a + 10;
+// alert (`Прибавляем 10 = ${b}`);
+// let c = b / 2;
+// alert (`Делим на 2 = ${c}`);
+// let result = c - userNum;
+// alert (result);
+// alert (`Ответ равен 5`);
