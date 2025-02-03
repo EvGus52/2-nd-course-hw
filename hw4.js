43ca6ccc99401463fb9b14359ded72586d7a3eff
EvGus52 <evgenijguskov247@gmail.com>
2025-02-03 22:17:06 +0300

    created hw4.js

---
+++ hw4.js
@@ -0,0 +1,51 @@
+//Задание № 1
+// for (let i = 0; i < 2; i++) {
+//     console.log(`Привет!`);
+//     }
+
+//Задание № 2
+// for (let i = 1; i <= 5; i++) {
+//         console.log(i);
+//         }
+
+//Задание № 3
+    // for (let i = 7; i >= 7 && i <= 22; i++) {
+    //     console.log(i);
+    //     }
+
+//Задание № 4
+// const obj = {
+//     "Коля" : '200',
+//     "Вася" : '300',
+//     "Петя" : '400'
+//     }
+//     for (let key in obj) {
+//     console.log(`${key}: ${obj[key]}`);
+//     }
+
+//Задание № 5
+    // let n = 1000;
+    // let num = 0;
+    // while (n >= 50) {
+    //     n /= 2;
+    //     num++;
+    //     }
+    // console.log("Результат деления: ", n);
+    // console.log("Количество итераций: ", num);
+
+//Задание № 6
+// for (let friday = 3; friday <= 31; friday +=7) {
+//     console.log(`Сегодня пятница ${friday}-е Января. Необходимо подготовить отчёт!`);
+//     }
+
+//Доп. задания
+
+//Задание № 1
+//  let k = 100;
+//     let iterations = 0;
+//     while (k > 0) {
+//         k -= 7;
+//         iterations++;
+//         }
+//     console.log("Результат вычитания: ", k);
+//     console.log("Количество итераций: ", iterations);
