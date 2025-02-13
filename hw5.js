7649bb088b90f33fe21f2a72be34771eb9607221
EvGus52 <evgenijguskov247@gmail.com>
2025-02-13 20:29:00 +0300

    completed five tasks js

---
+++ hw5.js
@@ -0,0 +1,67 @@
+// Задание 1
+
+// function minNumber(a, b) {
+//     if (a <= b) {
+//       return a;
+//     } else {
+//       return b;
+//     }
+//   }
+// let result = minNumber(8, 4);
+// console.log(`Меньшее из двух чисел равняется ${result}`);
+
+// Задание 2
+
+// function checkEvenNum(n) {
+//     if (n % 2 == 0) {
+//         return 'Число четное';
+//     }   else {
+//         return 'Число нечетное';
+//     }
+//   }
+// let result = checkEvenNum(8);
+// console.log(result);
+
+// Задание 3
+
+// let squareNum = (n) => {
+//     let result = n ** 2;
+//     console.log(`Квадрат числа равен ${result}`);
+//     return result;
+// }
+
+// squareNum(7);
+
+// Задание 4
+
+// function checkAge() {
+//     let age = Number(prompt("Введите ваш возраст"));
+
+//     if (age < 0) {
+//         alert(`Вы ввели неправильное значение`);
+//     } else if (age >= 0 && age <= 12) {
+//         alert(`Привет, друг!`)
+//     } else {
+//         alert(`Добро пожаловать!`)
+//     }   
+//   }
+  
+//   checkAge();
+
+// Задание 5
+
+// function mult(a, b) {
+//     if (isNaN(a) || isNaN(b)) {
+//         return `Одно или оба значения не являются числом`;
+//     } else {
+//         const result = a * b;
+//         return result;
+//     }
+// }
+
+// let num1 = prompt('Введите первое число');
+// let num2 = prompt('Введите второе число');
+// let result = mult(num1, num2);
+// console.log(`Результат умножения: ${result}`);
+
+// Задание 6
